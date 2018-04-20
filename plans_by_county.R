270de2b4a2c45ef4c6bc8d234d1dac0713913806
djangorris <lukkyjay@gmail.com>
2018-04-19 20:45:30 -0600

    Merged master and develop.

--- plans_by_county.R
+++ plans_by_county.R
@@ -10,8 +10,15 @@ library(purrr)
 library(knitr)
 library(ggplot2)
 library(tm)
+library(stringr)
 
 read <- readPDF(control = list(text = "-layout"))
 document <- Corpus(URISource("2018_plans.pdf"), readerControl = list(reader = read))
 doc <- content(document[[1]])
+doc <- stripWhitespace(doc)
+doc <- gsub("Individual Medical by Geographic Region", "", doc)
 head(doc)
+# Remove footer on first page
+# footer_row_1 <- grep("* Anthem Blue Cross/ Blue Shield (HMO Colorado) offers", doc)[1]
+# footer_row_2 <- grep("\\f", doc)[1] - 1
+#doc <- doc[-footer_row_1]
